05c0b252b07c2021d71e36a799fa3803efb65313
abiswas3 <aribiswas3@gmail.com>
2017-12-10 16:42:53 -0600

    Word2Vec being fixed

--- FeatureExtractionTools/word2vec.py
+++ FeatureExtractionTools/word2vec.py
@@ -6,20 +6,23 @@ import tensorflow as tf
 
 class Word2Vec(object):
 
-    def __init__(self, vocabulary, vocabulary_size):
+    def __init__(self,
+                 vocabulary,
+                 vocabulary_size,
+                 num_steps=10001,
+                 learning_rate=0.5):
 
         self.vocabulary_size = vocabulary_size
 
-        self.data, self.count, self.dictionary, self.reverse_dictionary = self.build_dataset(vocabulary,
-                                                                                             vocabulary_size)
+        self.data, self.count, self.dictionary, self.reverse_dictionary = self.build_dataset(vocabulary,vocabulary_size)
 
-        self.batch_size     = 128
-        self.embedding_size = 128     # Dimension of the embedding vector.
-        self.skip_window    = 1       # How many words to consider left and right.
-        self.num_skips      = 2       # How many times to reuse an input to generate a label.
-        self.num_sampled    = 64      # Number of negative examples to sample.
-
-
+        self.batch_size     = 1024
+        self.embedding_size = 64    # Dimension of the embedding vector.
+        self.skip_window    = 4       # How many words to consider left and right.
+        self.num_skips      = 4       # How many times to reuse an input to generate a label.
+        self.num_sampled    = 128     # Number of negative examples to sample.
+        self.num_steps      = num_steps
+        self.learning_rate = learning_rate
         self.graph = tf.Graph()
         
         self.create_network()
@@ -45,7 +48,6 @@ class Word2Vec(object):
         data = list()
         unk_count = 0
         for word in words:
-
             # if I can't find word: index of word = 0
             index = dictionary.get(word, 0)
             if index == 0:  # dictionary['UNK']
@@ -105,7 +107,7 @@ class Word2Vec(object):
                 labels[i * num_skips + j, 0] = buffer[context_word]
 
             if data_index == len(data):
-                buffer[:] = data[:span]
+                buffer = data[:span]
                 data_index = span
             else:
                 buffer.append(data[data_index])
@@ -117,12 +119,16 @@ class Word2Vec(object):
 
 
     def create_network(self):
-        
-        with self.graph.as_default():
-            
+
+        self.valid_size = 20     # Random set of words to evaluate similarity on.
+        self.valid_window = 10000  # Only pick dev samples in the head of the distribution.
+        self.valid_examples = np.random.choice(self.valid_window, self.valid_size, replace=False)
+
+        with self.graph.as_default():            
             self.train_inputs = tf.placeholder(tf.int32, shape=[self.batch_size])
             self.train_labels = tf.placeholder(tf.int32, shape=[self.batch_size, 1])
-
+            self.valid_dataset = tf.constant(self.valid_examples, dtype=tf.int32)
+            
             # Ops and variables pinned to the CPU because of missing GPU implementation
             with tf.device('/cpu:0'):
                 
@@ -159,25 +165,31 @@ class Word2Vec(object):
                 norm = tf.sqrt(tf.reduce_sum(tf.square(embeddings), 1, keep_dims=True))
                 self.normalized_embeddings = embeddings / norm
 
+                self.valid_embeddings = tf.nn.embedding_lookup(
+                    self.normalized_embeddings, self.valid_dataset)
+                
+                # Matrix multiply
+                self.similarity = tf.matmul(
+                    self.valid_embeddings, self.normalized_embeddings, transpose_b=True)
+                
                 # Everything in the graph that is variables will get initialized when we run this
                 self.init = tf.global_variables_initializer()
 
                 # Construct the SGD optimizer using a learning rate of 1.0.
-                self.optimizer = tf.train.GradientDescentOptimizer(1.0).minimize(self.loss)
+                self.optimizer = tf.train.GradientDescentOptimizer(self.learning_rate).minimize(self.loss)
 
 
 
     def train(self):
 
         data_index = 0
-        num_steps = 100001
 
         self.sess = tf.Session(graph=self.graph)
         self.init.run(session=self.sess)
         print('Initialized')
 
         average_loss = 0
-        for step in range(num_steps):
+        for step in range(self.num_steps):
             batch_inputs, batch_labels, data_index = self.generate_batch(self.data,
                                                                     self.count,
                                                                     self.dictionary,
@@ -189,7 +201,19 @@ class Word2Vec(object):
 
             feed_dict = {self.train_inputs: batch_inputs,
                          self.train_labels: batch_labels}
+            
+            ###############PRINT STUFF about training data#######################
+            # s = ['{} -> {}'.format(
+            #     self.reverse_dictionary[i],
+            #     self.reverse_dictionary[j[0]])
+            #      for i, j in zip(batch_inputs, batch_labels)]
+            
+            # for i in s:
+            #     print(i)
+            # print()
+            ###############PRINT STUFF about training data#######################
 
+            
             # We perform one update step by evaluating the optimizer op (including it
             # in the list of returned values for session.run()
 
@@ -199,15 +223,30 @@ class Word2Vec(object):
             if step % 2000 == 0:
                 if step > 0:
                     average_loss /= 2000
+                    
                     # The average loss is an estimate of the loss over the last 2000 batches.
                     print('Average loss at step ', step, ': ', average_loss)
                     average_loss = 0
 
-
             # Note that this is expensive (~20% slowdown if computed every 500 steps)
-            if step % 10000 == 0:
-                soln = self.normalized_embeddings.eval(session=self.sess)
 
+            if step % 20000 == 0:
+                print()
+                sim = self.similarity.eval(session=self.sess)
+                for i in range(self.valid_size):
+                    valid_word = self.reverse_dictionary[self.valid_examples[i]]
+                    top_k = 4  # number of nearest neighbors
+                    nearest = (-sim[i, :]).argsort()[1:top_k + 1]
+                    log_str = '%s:' % valid_word
+                    for k in range(top_k):
+                      close_word = self.reverse_dictionary[nearest[k]]
+                      log_str = '%s %s,' % (log_str, close_word)
+                      
+                    print(log_str)
+                    
+
+                
+        soln = self.normalized_embeddings.eval(session=self.sess)
         self.sess.close()
 
         self.word_embeddings = soln
